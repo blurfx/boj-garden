1d091a12e0abdf8cc8010f5d33fd6f12f37b7b8e
changhui lee <iam@xo.dev>
2021-03-26 19:57:39 +0900

    initial commit

---
+++ backend/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "ok")
+	})
+
+	e.Logger.Fatal(e.Start(":8000"))
+}
